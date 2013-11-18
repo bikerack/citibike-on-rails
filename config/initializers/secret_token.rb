695af863dbd52b697be01509dc5016319e619ff5
John Richardson <richardsonjm@gmail.com>
2013-11-18 18:53:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CitibikeOnRails::Application.config.secret_key_base = '59f470841f9f851f7cc7ca6641c9e4845f1e009b1c0804bf60f0a7bbb2d1ec50c414f4e72827e90acd6850ef009252208add193125025ebe33d552507aae87af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CitibikeOnRails::Application.config.session_store :cookie_store, key: '_citibike-on-rails_session'
