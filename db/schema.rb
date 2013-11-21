45c8f42f97eb7afdcf9aaff31225deaee0c64751
John Richardson <richardsonjm@gmail.com>
2013-11-21 14:05:46 -0500

    messing about

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120161330) do
+ActiveRecord::Schema.define(version: 20131121190431) do
 
   create_table "date_lookups", force: true do |t|
     t.datetime "history"
