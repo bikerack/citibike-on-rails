c0cfad947fd72f58f7be3f2b32a4a02c66ec7f20
casunlight <vivian.stanford@gmail.com>
2013-11-21 12:37:52 -0500

    fix time

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -25,12 +25,13 @@ class TripsController < ApplicationController
   # POST /trips
   # POST /trips.json
   def create
-    @origin = Origin.new(:address => trip_params[:origin][:address].concat(" NYC"))
+    @origin = Origin.new(:address => trip_params[:origin][:address].concat(" New York City"))
     @origin.save
-    @destination = Destination.new(:address => trip_params[:destination][:address].concat(" NYC"))
+    @destination = Destination.new(:address => trip_params[:destination][:address].concat(" New York City"))
     @destination.save
     @trip = Trip.new(:origin_id => @origin.id, :destination_id => @destination.id)
     @trip.save
+    @trip.start_time
 
     respond_to do |format|
       if @trip.save
