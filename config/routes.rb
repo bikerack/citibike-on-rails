23eb6a07e6307ace1d2dd9da31672506d6670439
John Richardson <richardsonjm@gmail.com>
2013-11-22 15:07:37 -0500

    working on destination bug

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ CitibikeOnRails::Application.routes.draw do
 
   resources :stations
 
+  root 'trips#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
