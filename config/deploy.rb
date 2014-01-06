b37e8544fc7257479bd7a5a26383b375df0531a2
John Richardson <richardsonjm@gmail.com>
2014-01-05 21:21:31 -0500

    turned off default-shell in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repository,  "git@github.com:bikerack/citibike-on-rails.git"
 
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
-set :default_shell, '/bin/bash -l'
+# set :default_shell, '/bin/bash -l'
 # require "rvm/capistrano"
 # require "bundler/capistrano"
 
